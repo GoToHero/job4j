d85be4f29481a509f44272feeb8aff08320bfe4a
mailto:go2heromad@gmail.com <go2heromad@gmail.com>
2018-08-31 20:25:24 +0600

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
-
+/**
+*Calculator for simple arithmetic operations.
+*@author GoToHero
+*@since 31.08.18
+*@version 1.0
+*/
 public class Calculate {
-
+	/**
+	*Main.
+	*Method for output of "useful" words.
+	*@param args - words.
+	*/
 	public static void main(String[] args)	{
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author GoToHero (mailto:go2heromad@gmail.com)
+* @version $Id$
+* @since 1.0
+*/
+package ru.job4j.calculate
