00cac52b0ca26f585aa3947111617da6687ee9e8
mailto:go2heromad@gmail.com <go2heromad@gmail.com>
2018-09-01 19:51:32 +0600

    1.4. JUnit

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -11,21 +11,18 @@ import static org.junit.Assert.assertThat;
 * @version $Id$
 * @since 1.0
 */
+
 public class CalculateTest {
 /**
 * Test echo.
 */
 @Test
-public void whenTakeNameThenTreeEchoPlusName() {
-	String input = "GoToHero";
-	String expect = "Echo, echo, echo : GoToHero";
-	
-	//Создание нового объекта.
-	Calculate calc = new Calculate();
-	
-	//Выполнение метода echo с параметром input и запись ее в переменную result.
-	String result = calc.echo(input);
-	assertThat(result, is(expect));
-}
+	public void whenTakeNameThenTreeEchoPlusName() {
+		String input = "GoToHero";
+		String expect = "Echo, echo, echo : GoToHero";
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
 
 }
