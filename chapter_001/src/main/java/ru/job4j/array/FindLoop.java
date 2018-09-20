f85f4a31eb03d87b50c76d4252ffb0d67454e180
mailto:go2heromad@gmail.com <go2heromad@gmail.com>
2018-09-20 21:44:36 +0600

    6.1. Классический поиск перебором.

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,9 +1,20 @@
 package ru.job4j.array;
-
+/**
+ *  @author GoToHero
+ *  @version $Id$
+ *  @since 0.1
+ */
 public class FindLoop {
+    /**
+     * Метод для поиска заданного значения в массиве целых чисел.
+     * @param data задание массива.
+     * @param el задание искомого значения.
+     * @return вывод номера элемента массива, при совпадении.
+     */
     public int indexOf(int[] data, int el) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
-        for (int index : data) {
+        int index;
+        for (index = 0; index != data.length; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
