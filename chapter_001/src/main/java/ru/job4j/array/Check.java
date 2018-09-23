0b1ff6493d1046d9ba25a09491a6671c2a662b38
mailto:go2heromad@gmail.com <go2heromad@gmail.com>
2018-09-24 00:03:24 +0600

    6.3. Массив заполнен true или false

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+/**
+ *  @author GoToHero
+ *  @version $Id$
+ *  @since 0.1
+ */
+public class Check {
+    /**
+     * Метод проверяющий массив на одинаковые значения истинности.
+     * @param data проверяемый массив.
+     * @return результат, показывающий все ли значения массива истинны или ложны.
+     */
+    public boolean mono(boolean[] data) {
+        boolean result = false;
+        int index;
+        boolean boolTrue = true;
+        boolean boolFalse = false;
+        for (index = 0; index != data.length - 1; index++) {
+        boolTrue = boolTrue & data[index];
+        boolFalse = boolFalse | data[index];
+        }
+        if (boolTrue == true) {
+            result = true;
+        }
+        if (boolFalse == false) {
+            result = true;
+        }
+        return result;
+    }
+}
