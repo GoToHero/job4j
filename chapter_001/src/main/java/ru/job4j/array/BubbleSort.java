a9afc5af8caa051b39c90a9c4dfdcadce4ad69b7
mailto:go2heromad@gmail.com <go2heromad@gmail.com>
2018-09-27 20:07:26 +0600

    6.5. Создать программу для сортировки массива методом перестановки.

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -13,6 +13,20 @@ public class BubbleSort {
     public static int[] sort(int[] array) {
         int temp;
         for (int lvl = 1; lvl < array.length; lvl++) {
+            for (int index = 0; index < array.length - lvl; index++) {
+                if (array[index] > array[index + 1]) {
+                    temp = array[index];
+                    array[index] = array[index + 1];
+                    array[index + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+    /* Первоначальный вариант отпадает, потому что метод проверяет уже отсортированные значения массива.
+    public static int[] sort(int[] array) {
+        int temp;
+        for (int lvl = 1; lvl < array.length; lvl++) {
             for (int index = 1; index < array.length; index++) {
                 if (array[index - 1] > array[index]) {
                     temp = array[index - 1];
@@ -22,7 +36,7 @@ public class BubbleSort {
             }
         }
     return array;
-    }
+    }*/
 }
 
 //Если что попробовать вариант с занесением условия когда последнее число больше первого в цикл.
