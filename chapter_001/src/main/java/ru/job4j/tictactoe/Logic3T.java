05962b50572e1cef67159d281b5c5f51410ee159
mailto:go2heromad@gmail.com <go2heromad@gmail.com>
2019-01-13 20:22:56 +0600

    7. Крестики-нолики на JavaFX.

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -8,64 +8,76 @@ public class Logic3T {
     }
 
     public boolean isWinnerX() {
-        boolean result = true;
-        for (int i = 0; i < this.table.length - 1; i++) {
-            for (int j = 0; j < this.table.length - 1; j++) {
-                if (this.table[i][j].hasMarkX() != this.table[i][j + 1].hasMarkX()) {
-                    result = false;
-                    break;
+        boolean result = false;
+        int xStripeX = 0;
+        int yStripeX = 0;
+        int downDiagStripeX = 0;
+        int upDiagStripeX = 0;
+        for (int i = 0; i < this.table.length; i++) {
+            for (int j = 0; j < this.table.length; j++) {
+                if (this.table[i][j].hasMarkX()) {
+                    xStripeX++;
+                } else {
+                    xStripeX = 0;
+                }
+                if (this.table[j][i].hasMarkX()) {
+                    yStripeX++;
+                } else {
+                    yStripeX = 0;
                 }
             }
-        }
-        if (!result) {
-            for (int i = 0; i < this.table.length - 1; i++) {
-                for (int j = 0; j < this.table.length - 1; j++) {
-                    if (this.table[j][i].hasMarkX() != this.table[j + 1][i].hasMarkX()) {
-                        result = false;
-                        break;
-                    }
-                }
+            if (this.table[i][i].hasMarkX()) {
+                downDiagStripeX++;
+            } else {
+                downDiagStripeX = 0;
             }
-        }
-        if (!result) {
-            for (int i = 0; i < this.table.length - 1; i++) {
-                if (this.table[i][i].hasMarkX() != this.table[i + 1][i + 1].hasMarkX()
-                        && this.table[this.table.length - 1 - i][i].hasMarkX() != this.table[this.table.length - 2 - i][i + 1].hasMarkX()) {
-                    result = false;
-                    break;
-                }
+            if (this.table[i][this.table.length - i - 1].hasMarkX()) {
+                upDiagStripeX++;
+            } else {
+                upDiagStripeX = 0;
+            }
+            if (xStripeX == this.table.length || yStripeX == this.table.length ||
+                    downDiagStripeX == this.table.length || upDiagStripeX == this.table.length) {
+                result = true;
+                break;
             }
         }
         return result;
     }
 
     public boolean isWinnerO() {
-        boolean result = true;
-        for (int i = 0; i < this.table.length - 1; i++) {
-            for (int j = 0; j < this.table.length - 1; j++) {
-                if (this.table[i][j].hasMarkO() != this.table[i][j + 1].hasMarkO()) {
-                    result = false;
-                    break;
+        boolean result = false;
+        int xStripeO = 0;
+        int yStripeO = 0;
+        int downDiagStripeO = 0;
+        int upDiagStripeO = 0;
+        for (int i = 0; i < this.table.length; i++) {
+            for (int j = 0; j < this.table.length; j++) {
+                if (this.table[i][j].hasMarkO()) {
+                    xStripeO++;
+                } else {
+                    xStripeO = 0;
+                }
+                if (this.table[j][i].hasMarkO()) {
+                    yStripeO++;
+                } else {
+                    yStripeO = 0;
                 }
             }
-        }
-        if (!result) {
-            for (int i = 0; i < this.table.length - 1; i++) {
-                for (int j = 0; j < this.table.length - 1; j++) {
-                    if (this.table[j][i].hasMarkO() != this.table[j + 1][i].hasMarkO()) {
-                        result = false;
-                        break;
-                    }
-                }
+            if (this.table[i][i].hasMarkO()) {
+                downDiagStripeO++;
+            } else {
+                downDiagStripeO = 0;
             }
-        }
-        if (!result) {
-            for (int i = 0; i < this.table.length - 1; i++) {
-                if (this.table[i][i].hasMarkO() != this.table[i + 1][i + 1].hasMarkO()
-                        || this.table[this.table.length - 1 - i][i].hasMarkO() != this.table[this.table.length - 2 - i][i + 1].hasMarkO()) {
-                    result = false;
-                    break;
-                }
+            if (this.table[i][this.table.length - i - 1].hasMarkO()) {
+                upDiagStripeO++;
+            } else {
+                upDiagStripeO = 0;
+            }
+            if (xStripeO == this.table.length || yStripeO == this.table.length ||
+                    downDiagStripeO == this.table.length || upDiagStripeO == this.table.length) {
+                result = true;
+                break;
             }
         }
         return result;
