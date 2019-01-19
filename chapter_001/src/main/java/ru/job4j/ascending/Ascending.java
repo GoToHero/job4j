c0528370146c95ec574e8d73fa7e05e351ea4fcd
GoToHero <go2heromad@gmail.com>
2019-01-19 18:56:03 +0600

    7. Сортировка двух массивов в один массив.

--- chapter_001/src/main/java/ru/job4j/ascending/Ascending.java
+++ chapter_001/src/main/java/ru/job4j/ascending/Ascending.java
@@ -10,19 +10,51 @@ public class Ascending {
     private static int[] secondArray;
 
     public static int[] increasingSort (int[] firstArray, int[] secondArray) {
-        int x;
-        int y;
-        int arraySize = firstArray.length + secondArray.length;
-        int[] increasArray = new int [arraySize];
-        for (x = 0; x < firstArray.length; x++) {
-            for (y = 0; y < secondArray.length; y++) {
-                if (secondArray[x] <= firstArray[y]) {
-                    increasArray[step] = secondArray[x];
-                    increasArray[step + 1] = ;
-                }
-
+        int n = firstArray.length;
+        int m = secondArray.length;
+        int[] increasArray = new int [n + m];
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        while (i < n && j < m) {
+            if (firstArray[i] < secondArray[j]) {
+                increasArray[index] = firstArray[i];
+                i++;
+            }
+            else {
+                increasArray[index] = secondArray[j];
+                j++;
+            }
+            index++;
+        }
+        while (i < n) {
+            increasArray[index] = firstArray[i];
+            index++;
+            i++;
+        }
+        while (j < m) {
+            increasArray[index] = secondArray[j];
+            index++;
+            j++;
+        }
+        /*for (i, j; i < n && j < m; index++) {
+            if (firstArray[i] < secondArray[j]) {
+                increasArray[index] = firstArray[i];
+                i++;
+            }
+            else {
+                increasArray[index] = secondArray[j];
+                j++;
             }
         }
+        for (i; i < n; index++) {
+            increasArray[index] = firstArray[i];
+            i++;
+        }
+        for (j; j < m; index++) {
+            increasArray[index] = secondArray[j];
+            j++;
+        }*/
         return increasArray;
     }
 }
--- chapter_001/src/test/java/ru/job4j/ascending/AscendingTest.java
+++ chapter_001/src/test/java/ru/job4j/ascending/AscendingTest.java
@@ -15,4 +15,22 @@ public class AscendingTest {
         int[] expect = new int [] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         assertThat(result, is(expect));
     }
+    @Test
+    public void whenWeHaveTwoDifferentSizeArrays() {
+        Ascending ascending = new Ascending();
+        int[] firstArray = new int[] {1, 2, 3};
+        int[] secondArray = new int[] {4, 5, 6, 7, 8, 9, 10};
+        int[] result = Ascending.increasingSort(firstArray, secondArray);
+        int[] expect = new int [] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenWeHaveTwoIntersectingArrays() {
+        Ascending ascending = new Ascending();
+        int[] firstArray = new int[] {1, 2, 3, 4, 5};
+        int[] secondArray = new int[] {3, 4, 5, 6, 7};
+        int[] result = Ascending.increasingSort(firstArray, secondArray);
+        int[] expect = new int [] {1, 2, 3, 3, 4, 4, 5, 5, 6, 7};
+        assertThat(result, is(expect));
+    }
 }
