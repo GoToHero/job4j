1db491888a9fb74685ff3654dedd0b5671821c0a
GoToHero <go2heromad@gmail.com>
2019-03-11 21:40:14 +0600

    1.2.4.4 Используя шаблон проектирования - стратегию.

--- chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
+++ chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
@@ -1,7 +1,9 @@
 package ru.job4j.pseudo;
 
 public class Paint {
-    public void draw(Shape shape) {
+    private Shape shape;
 
+    public void draw(Shape shape) {
+        System.out.println(shape.draw());
     }
 }
--- chapter_002/src/main/java/ru/job4j/pseudo/Shape.java
+++ chapter_002/src/main/java/ru/job4j/pseudo/Shape.java
@@ -1,5 +1,5 @@
 package ru.job4j.pseudo;
 
 public interface Shape {
-    public String draw();
+    String draw();
 }
