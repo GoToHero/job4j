42e321f8998d198ceb7c4b3b85f7d9a0aca91438
GoToHero <go2heromad@gmail.com>
2019-04-06 11:15:38 +0600

    1.2.6.1. Обеспечить бесперебойную работу приложения Tracker.

--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -9,11 +9,9 @@ public class ValidateInput extends ConsoleInput {
             try {
                 value = super.ask(question, range);
                 invalid = false;
-            }
-            catch (MenuOutException moe) {
+            } catch (MenuOutException moe) {
                 System.out.println("Please select key from menu.");
-            }
-            catch (NumberFormatException nfe) {
+            } catch (NumberFormatException nfe) {
                 System.out.println("Please enter validate data again");
             }
         } while (invalid);
